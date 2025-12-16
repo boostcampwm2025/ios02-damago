32f252632c9a11c61d23ebc9dc2df5483d79ef9c
wodud1107 <wodud7992@naver.com>
2025-12-16 16:24:58 +0900

    chore: 프로젝트 생성

---
+++ Damago/Damago/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Damago
+//
+//  Created by 김재영 on 12/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
