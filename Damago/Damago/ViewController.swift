abfded7600b9a9dd9db692d781ace7b36927fa75
loyH <gkwhdtjr20@naver.com>
2025-12-16 22:19:48 +0900

    [#8] feat: Gifu를 이용한 gif 재생

--- Damago/Damago/ViewController.swift
+++ Damago/Damago/ViewController.swift
@@ -6,10 +6,30 @@
 //
 
 import UIKit
+import Gifu
 
-class ViewController: UIViewController {    
+@MainActor
+final class ViewController: UIViewController {
+    private let gifImageView: GIFImageView = {
+        let imageView = GIFImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(gifImageView)
+        
+        NSLayoutConstraint.activate([
+            gifImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            gifImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            gifImageView.widthAnchor.constraint(equalToConstant: 200),
+            gifImageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        gifImageView.animate(withGIFNamed: "dog")
     }
 }
