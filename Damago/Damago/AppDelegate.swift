0a109e7568b4341febba115be722d72cb016c301
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-12-17 20:53:55 +0900

    [#14] fix: pbxproj 충돌 해결 (#15)

--- Damago/Damago/AppDelegate.swift
+++ Damago/Damago/AppDelegate.swift
@@ -57,7 +57,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
 
