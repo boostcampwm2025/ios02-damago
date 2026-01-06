14db0eaf654a6cc967529d394a19444441fdfcbe
loyH <gkwhdtjr20@naver.com>
2026-01-06 21:08:55 +0900

    [#28] design: font 추가

---
+++ Damago/Shared/Font+.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+extension UIFont {
+    static var title1: UIFont {
+        .systemFont(ofSize: 26, weight: .bold)
+    }
+
+    static var title2: UIFont {
+        .systemFont(ofSize: 24, weight: .semibold)
+    }
+
+    static var title3: UIFont {
+        .systemFont(ofSize: 20, weight: .bold)
+    }
+
+    static var body1: UIFont {
+        .systemFont(ofSize: 18, weight: .bold)
+    }
+
+    static var body2: UIFont {
+        .systemFont(ofSize: 16, weight: .semibold)
+    }
+
+    static var body3: UIFont {
+        .systemFont(ofSize: 14, weight: .medium)
+    }
+
+    static var caption: UIFont {
+        .systemFont(ofSize: 12, weight: .medium)
+    }
+}
