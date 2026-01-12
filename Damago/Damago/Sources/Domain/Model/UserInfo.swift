75c05499c628bb2fe1bd10ec3926b31fce7bfb6c
wodud1107 <wodud7992@naver.com>
2026-01-13 02:17:43 +0900

    [#49] feat: 도메인 모델 생성
    - UserInfo: 유저 정보
    - PetStatus: 펫 정보

---
+++ Damago/Damago/Sources/Domain/Model/UserInfo.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfo.swift
+//  Damago
+//
+//  Created by 김재영 on 1/12/26.
+//
+
+import Foundation
+
+struct UserInfo {
+    let udid: String
+    let damagoID: String?
+    let partnerUDID: String?
+    let nickname: String?
+    let petStatus: PetStatus?
+    let lastFedAt: Date?
+}
