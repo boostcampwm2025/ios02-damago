b236b64b2b7571933e7503e1ccb62105a6fcbb0d
wodud1107 <wodud7992@naver.com>
2026-01-14 14:46:05 +0900

    [#87] chore: firebase 프로젝트 환경 분리
    - 개발환경 damago-dev-26
    - 운영환경 damago-a43da
    - Run Script로 GoogleService-Info.plist 구분

--- Damago/DamagoNetwork/Sources/DamagoNetwork/Network/BaseURL.swift
+++ Damago/DamagoNetwork/Sources/DamagoNetwork/Network/BaseURL.swift
@@ -9,10 +9,17 @@ import Foundation
 
 public enum BaseURL {
     public static var string: String {
+        #if DEBUG
+        // Local Emulator
         if let localIP = ProcessInfo.processInfo.environment["USE_LOCAL_EMULATOR"] {
             return "http://\(localIP):5001/damago-a43da/us-central1"
         }
         
+        // DEV
+        return "https://us-central1-damago-dev-26.cloudfunctions.net"
+        #else
+        // PROD
         return "https://us-central1-damago-a43da.cloudfunctions.net"
+        #endif
     }
 }
