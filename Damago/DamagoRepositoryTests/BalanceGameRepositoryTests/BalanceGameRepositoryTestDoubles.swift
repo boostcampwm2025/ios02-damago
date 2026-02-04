ee2d350b2931e736ac6339223f83e356832d07d4
Landelyse <wlsghd70@gmail.com>
2026-02-04 20:13:09 +0900

    [#296] feat: 밸런스게임 테스트용 테스트 더블 정의

---
+++ Damago/DamagoRepositoryTests/BalanceGameRepositoryTests/BalanceGameRepositoryTestDoubles.swift
@@ -0,0 +1,48 @@
+//
+//  BalanceGameRepositoryTestDoubles.swift
+//  DamagoRepositoryTests
+//
+//  Created by Eden Landelyse on 2/4/26.
+//
+
+import Foundation
+import Combine
+import SwiftData
+@testable import Damago
+@testable import DamagoNetwork
+
+@MainActor
+final class MockBalanceGameLocalDataSource: BalanceGameLocalDataSourceProtocol, @unchecked Sendable {
+    var fetchGameResult: BalanceGameEntity?
+    var fetchGameError: Error?
+    
+    var fetchLatestGameHandler: (() async throws -> BalanceGameEntity?)?
+    var saveGameCalledWith: BalanceGameEntity?
+    
+    var updateChoiceCalledWith: (gameID: String, user1Choice: Int?, user2Choice: Int?, lastAnsweredAt: Date?)?
+    var updateChoiceError: Error?
+    
+    func fetchGame(id: String) async throws -> BalanceGameEntity? {
+        if let error = fetchGameError { throw error }
+        return fetchGameResult
+    }
+
+    func fetchLatestGame() async throws -> BalanceGameEntity? {
+        if let error = fetchGameError { throw error }
+        return try await fetchLatestGameHandler?()
+    }
+
+    func saveGame(_ entity: BalanceGameEntity) async throws {
+        saveGameCalledWith = entity
+    }
+
+    func updateChoice(
+        gameID: String,
+        user1Choice: Int?,
+        user2Choice: Int?,
+        lastAnsweredAt: Date?
+    ) async throws {
+        if let error = updateChoiceError { throw error }
+        updateChoiceCalledWith = (gameID, user1Choice, user2Choice, lastAnsweredAt)
+    }
+}
