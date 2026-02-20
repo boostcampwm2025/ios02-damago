25f9cf4ab8186072dc06da1944289b5ca6272388
wodud1107 <wodud7992@naver.com>
2026-02-20 19:41:45 +0900

    [#343] fix: 테스트 불안정 해소
    - Task 를 실행하고 잊은 뒤 defer 가 호출 되므로 Task 내에서 호출하도록 수정

--- Damago/Damago/Sources/Presentation/Features/Collection/CollectionViewModel.swift
+++ Damago/Damago/Sources/Presentation/Features/Collection/CollectionViewModel.swift
@@ -90,8 +90,8 @@ final class CollectionViewModel: ViewModel {
     private func changeDamago() {
         guard let selectedDamago = state.selectedDamago else { return }
 
+        state.isLoading = true
         Task {
-            state.isLoading = true
             defer { state.isLoading = false }
 
             do {
